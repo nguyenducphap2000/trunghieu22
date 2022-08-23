c634a8e43e77fc229671757a2058a139a4b225a8
= <nguyenducphap2000@gmail.com>
2022-08-23 18:48:33 +0700

    aa

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
 
 window.requestAnimFrame = function () {
   return window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function (callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    };
 }();
 
 // now we will setup our basic variables for the demo
 var canvas = document.getElementById('canvas'),
-ctx = canvas.getContext('2d'),
-// full screen dimensions
-cw = window.innerWidth,
-ch = window.innerHeight,
-// firework collection
-fireworks = [],
-// particle collection
-particles = [],
-// starting hue
-hue = 120,
-// when launching fireworks with a click, too many get launched at once without a limiter, one launch per 5 loop ticks
-limiterTotal = 5,
-limiterTick = 0,
-// this will time the auto launches of fireworks, one launch per 80 loop ticks
-timerTotal = 80,
-timerTick = 0,
-mousedown = false,
-// mouse x coordinate,
-mx,
-// mouse y coordinate
-my;
+  ctx = canvas.getContext('2d'),
+  // full screen dimensions
+  cw = window.innerWidth,
+  ch = window.innerHeight,
+  // firework collection
+  fireworks = [],
+  // particle collection
+  particles = [],
+  // starting hue
+  hue = 120,
+  // when launching fireworks with a click, too many get launched at once without a limiter, one launch per 5 loop ticks
+  limiterTotal = 5,
+  limiterTick = 0,
+  // this will time the auto launches of fireworks, one launch per 80 loop ticks
+  timerTotal = 80,
+  timerTick = 0,
+  mousedown = false,
+  // mouse x coordinate,
+  mx,
+  // mouse y coordinate
+  my;
 
 // set canvas dimensions
 canvas.width = cw;
@@ -46,7 +46,7 @@ function random(min, max) {
 // calculate the distance between two points
 function calculateDistance(p1x, p1y, p2x, p2y) {
   var xDistance = p1x - p2x,
-  yDistance = p1y - p2y;
+    yDistance = p1y - p2y;
   return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
@@ -98,7 +98,7 @@ Firework.prototype.update = function (index) {
 
   // get the current velocities based on angle and speed
   var vx = Math.cos(this.angle) * this.speed,
-  vy = Math.sin(this.angle) * this.speed;
+    vy = Math.sin(this.angle) * this.speed;
   // how far will the firework have traveled with velocities applied?
   this.distanceTraveled = calculateDistance(this.sx, this.sy, this.x + vx, this.y + vy);
 
@@ -287,14 +287,13 @@ function reveal() {
 
   var w, h;
   if (window.innerWidth >= 1000) {
-    w = 295;h = 185;
-  } else
-  {
-    w = 255;h = 155;
+    w = 295; h = 185;
+  } else {
+    w = 255; h = 155;
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/gbICivOO26U?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://youtube.com/embed/0EuIWK3-4uU?feature=share?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
